2062a6f06dd114b8e7fe1f8a8ca05d2d78ffdbbc
Mccranky83 <ericcai20021118@gmail.com>
2024-10-23 20:44:43 +0800

    Create README.md and meta file

---
+++ README.md
@@ -0,0 +1,19 @@
+# Dynasty Scans Batch Downloader
+
+Download doujinshi from [Dynasty Scans](https://dynasty-scans.com/) as zip archives :package:
+
+## Preview
+
+![Preview](https://raw.githubusercontent.com/Mccranky83/dynasty-dl/main/assets/Preview.png)
+
+## Installation
+
+- [Download from GitHub](https://github.com/Mccranky83/dynasty-dl/raw/main/dynasty-dl.user.js)
+- [Download from GreasyFork](https://update.greasyfork.org/scripts/513624/Dynasty%20Scans%20Batch%20Downloader.user.js)
+
+## Usage
+
+1. Tick off the chapters you want to download.
+2. Press `Download all`
+3. Coffee break ~ :coffee:
+4. Return to find the goods in `~/Downloads`
--- dynasty-dl.user.js
+++ dynasty-dl.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Dynasty Scans Batch Downloader
 // @namespace    mccranky83.github.io
-// @version      2024-10-22
+// @version      2024-10-23
 // @description  Download doujinshi from Dynasty Scans
 // @author       Mccranky83
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js
 // @match        https://dynasty-scans.com/series/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dynasty-scans.com
+// @downloadURL  https://github.com/Mccranky83/dynasty-dl/raw/main/dynasty-dl.user.js
+// @updateURL    https://github.com/Mccranky83/dynasty-dl/raw/main/dynasty-dl.user.js
 // @grant        none
 // @license      MIT
 // ==/UserScript==
